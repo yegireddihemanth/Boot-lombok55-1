5208c1e93047484a922fe9993c8f2630a3a6728b
hemanth <hemanthyegireddyad@gmail.com>
2022-05-17 09:15:45 +0530

    done

---
+++ Boot-lombok55-1/src/main/java/com/jobiak/lombok/BootLombok551Application.java
@@ -0,0 +1,13 @@
+package com.jobiak.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLombok551Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLombok551Application.class, args);
+	}
+
+}
---
+++ Boot-lombok55-1/src/test/java/com/jobiak/lombok/BootLombok551ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobiak.lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLombok551ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
